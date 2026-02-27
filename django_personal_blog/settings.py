defa1bba197eef6206e81f1174e9cd88ff04b6a8
mina-y-Khalil <mina.y.khalil90@gmail.com>
2026-02-26 16:11:40 -0800

    feat: add static files and update templates for improved styling

--- django_personal_blog/settings.py
+++ django_personal_blog/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  #add this for auto-detecting the app and static files on project level
+]
